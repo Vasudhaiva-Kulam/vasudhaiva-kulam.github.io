e862885e175457e8ab73e6a7adc1f2c30b5258ec
Lakshya Keshwani <lakshyakeshwani3@gmail.com>
2026-09-06 19:11:00 +0530

    add custom deploy workflow and change astro build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: "https://vasudhaiva-kulam.github.io",
+  base: "/website", // only needed for a project page, not a custom domain
+  vite: { plugins: [tailwindcss()] },
 });
