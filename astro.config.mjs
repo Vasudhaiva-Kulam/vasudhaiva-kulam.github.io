6fdff719f696640eadcda0376335981fb364193c
Lakshya Keshwani <lakshyakeshwani3@gmail.com>
2026-09-13 00:59:36 +0530

    remove base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vasudhaiva-kulam.github.io",
-  base: "/website", // only needed for a project page, not a custom domain
+  base: "",
   vite: { plugins: [tailwindcss()] },
 });
